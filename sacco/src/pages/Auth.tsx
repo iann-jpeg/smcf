a6bb2d35c18f1c296bb4815c850c8a9bb7025e94
iann-jpeg <iann-jpeg@github.com>
2026-04-09 00:23:50 +0300

    Use email and password for SACCO login

--- sacco/src/pages/Auth.tsx
+++ sacco/src/pages/Auth.tsx
@@ -100,7 +100,7 @@ export default function Auth() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          username: identifierValue,
+          email: identifierValue,
           password,
         }),
       });
